9bce104ddad68bf8244b64ea97908b44d1cface6
affli <delycid@gmail.com>
2010-11-11 03:05:13 +0300

    start over. blah.

--- core/init.lua
+++
@@ -1,5 +0,0 @@
-local ui, namespace = ...
-
-namespace[1] = {} -- F, library
-namespace[2] = {} -- C, config
-namespace[3] = {} -- L, localization
--- elements/tooltip.lua
+++
@@ -1,3 +0,0 @@
-GameTooltip:HookScript("OnShow",function(self)if InCombatLockdown()then self:Hide()else self:SetTemplate()end end)
-GameTooltipStatusBar:SetScript("OnShow",function(self)self:Die()end)
-hooksecurefunc("GameTooltip_SetDefaultAnchor",function(self,parent)self:SetOwner(parent,"ANCHOR_CURSOR")self.default=1 end)
